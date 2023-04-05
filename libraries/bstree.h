49857931fe480b311a7d227f18e3f1b66d808bb6
Master <mr_vidali@mail.ru>
2023-04-05 20:15:01 +0700

    Fifth experiment

--- libraries/bstree.h
+++ libraries/bstree.h
@@ -8,6 +8,7 @@ struct bstree {
   struct bstree *right;
 };
 
+void bstree_add_right(struct bstree *tree, char *key, int value);
 struct bstree *bstree_create(char *key, int value);
 void bstree_add(struct bstree *tree, char *key, int value);
 struct bstree *bstree_lookup(struct bstree *tree, char *key);
