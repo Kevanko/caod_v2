470d6c87027cc3945c3b5cda1a4b43b379bc6e9d
Master <mr_vidali@mail.ru>
2023-04-12 00:12:20 +0700

    Ex1

--- src/main.c
+++ src/main.c
@@ -8,72 +8,67 @@
 #include "../libraries/hashtab.h"
 #include "../libraries/header.h"
 
-char words_bad[Max_Keys][Max_Key_Length];
-char words_good[Max_Keys][Max_Key_Length];
+struct listnode *hashtab[Hashtab_Size];
+char words[Max_Keys][Max_Key_Length];
 
 int main() {
 
-  struct bstree *tree_good, *tree_bad, *node_good, *node_bad;
-  double time_good = 0.0, time_bad = 0.0;
+  struct bstree *tree, *node_tree;
+  struct listnode *hash, *node_hash;
+  double time_tree = 0.0, time_hash = 0.0;
   clock_t begin, end;
 
-  //---------Read-Bad---------//
-  FILE *file = fopen("keys2.txt", "r");
+  //---------Read-Words---------//
+  FILE *file = fopen("keys.txt", "r");
   for (int i = 0; i < Max_Keys; i++) {
-    fgets(words_bad[i], Max_Key_Length, file);
-    words_bad[i][strlen(words_bad[i]) - 1] = '\0';
+    fgets(words[i], Max_Key_Length, file);
+    words[i][strlen(words[i]) - 2] = '\0';
   }
   fclose(file);
-  //---------Read-Bad---------//
+  //---------Read-Words---------//
 
-  //---------Read-Good---------//
-  file = fopen("keys.txt", "r");
-  for (int i = 0; i < Max_Keys; i++) {
-    fgets(words_good[i], Max_Key_Length, file);
-    words_good[i][strlen(words_good[i]) - 2] = '\0';
-  }
-  fclose(file);
-  //---------Read-Good---------//
-  printf(FG_CYAN "\t\t\tExperiment #5 --> Bstree_max (Bad) vs Bstree_max (Good) "
+  printf(FG_CYAN "\t\t\tExperiment #1 --> Bstree_Lookup vs Hashtab_Lookup <--"
                  "<--\n" FG_WHITE);
 
   print_cap(121, '-');
-  printf(FG_YELLOW "#\t" FG_CYAN "|  " FG_GREEN "time_good\t" FG_CYAN
-                   "| " FG_RED " time_bad\t" FG_CYAN "|\t\t" FG_GREEN
-                   "key_good\t\t" FG_CYAN "|\t\t" FG_RED "key_bad\t\t\t" FG_CYAN
+  printf(FG_YELLOW "#\t" FG_CYAN "|  " FG_GREEN "time_hash\t" FG_CYAN
+                   "| " FG_RED " time_tree\t" FG_CYAN "|\t\t" FG_GREEN
+                   "key_hash\t\t" FG_CYAN "|\t\t" FG_RED "key_tree\t\t" FG_CYAN
                    "|\n" FG_WHITE);
   print_cap(121, '-');
 
-  tree_good = bstree_create(words_good[0], 0);
-  tree_bad = bstree_create(words_bad[0], 0);
+  hashtab_init(hashtab);
+  hashtab_add(hashtab, words[0], 0);
+  tree = bstree_create(words[0], 0);
 
   for (int i = 1; i < Max_Keys; i++) {
-    bstree_add(tree_good, words_good[i], i);
-    bstree_add(tree_bad, words_bad[i], i);
-    if ((i + 1) % 10000 == 0) {
+    bstree_add(tree, words[i], i);
+    hashtab_add(hashtab, words[i], i);
 
+    if ((i + 1) % 10000 == 0) {
+      char *rand_key = words[getrand(0, i)];
       //---------Good---------//
       begin = clock();
-      node_good = bstree_max(tree_good);
+      node_tree = bstree_lookup(tree, rand_key);
       end = clock();
-      time_good = (double)(end - begin) / CLOCKS_PER_SEC;
+      time_tree = (double)(end - begin) / CLOCKS_PER_SEC;
       //---------Good---------//
 
       //---------Bad---------//
       begin = clock();
-      node_bad = bstree_max(tree_bad);
+      node_hash = hashtab_lookup(hashtab, rand_key);
       end = clock();
-      time_bad = (double)(end - begin) / CLOCKS_PER_SEC;
+      time_hash = (double)(end - begin) / CLOCKS_PER_SEC;
       //---------Bad---------//
 
       printf(FG_YELLOW "%d\t" FG_CYAN "| " FG_GREEN "%f\t" FG_CYAN "| " FG_RED
                        "%f\t" FG_CYAN "|" FG_GREEN "%35.35s\t" FG_CYAN
                        "|" FG_RED " %35.35s\t" FG_CYAN "|\n" FG_WHITE,
-             i + 1, time_good, time_bad, node_good->key, node_bad->key);
+             i + 1, time_hash, time_tree, node_hash->key, node_tree->key);
     }
   }
   print_cap(121, '-');
-  bstree_free(tree_good);
-  bstree_free(tree_bad);
+  bstree_free(tree);
+  free_table(hashtab);
   return 0;
 }
